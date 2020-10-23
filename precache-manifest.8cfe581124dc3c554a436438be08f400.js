a0a8eec9ee01c75fe6b94fff6e2a890de2c33ec9
henk <hkripper@gmail.com>
2020-10-23 11:30:04 +0200

    Updates

---
+++ precache-manifest.8cfe581124dc3c554a436438be08f400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5b2bce68f754201f05272c5eb48223",
+    "url": "/everydayCalendar/index.html"
+  },
+  {
+    "revision": "221619abadcf29cc8ef0",
+    "url": "/everydayCalendar/static/css/main.04af04bf.chunk.css"
+  },
+  {
+    "revision": "80c9f556b39bf574d7bc",
+    "url": "/everydayCalendar/static/js/2.7298b194.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/everydayCalendar/static/js/2.7298b194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "221619abadcf29cc8ef0",
+    "url": "/everydayCalendar/static/js/main.e2dd6622.chunk.js"
+  },
+  {
+    "revision": "19e75a9e3cca722ff2e0",
+    "url": "/everydayCalendar/static/js/runtime-main.e821f5ee.js"
+  }
+]);
